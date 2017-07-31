64af09c611195e209444831bb3d22053d3e5d1e3
Mike Schutte <tmikeschutte@gmail.com>
2017-07-31 15:24:11 -0600

    Add shoulda, factory girl

--- dev_gems.rb
+++ dev_gems.rb
@@ -1,9 +1,8 @@
 group :development, :test do
-  gem 'pry'
+  gem 'pry-state'
   gem 'rspec-rails', '~> 3.5'
-  gem 'factory_girl_rails'
-  gem 'shoulda-matchers', '~> 3.1'
+  gem "factory_girl_rails", "~> 4.0"
+  gem 'shoulda-matchers', git: 'https://github.com/thoughtbot/shoulda-matchers.git', branch: 'rails-5'
   gem 'capybara'
-  gem 'launchy'
 end
 
